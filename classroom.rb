6f52b908adbd041a27dcfdf43a8846655c39fba0
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-08-03 18:56:04 +0100

    Add Both ways relationships

--- classroom.rb
+++ classroom.rb
@@ -1,7 +1,14 @@
-class classroom
-    def initialize(label)
-        @label = label
-    end
+class Classroom
+  def initialize(label)
+    @label = label
+    @students = []
+  end
 
-    attr_accessor :label
-end 
+  attr_accessor :label
+  attr_reader :students
+
+  def add_students(student)
+    @students.push(student)
+    student.owner = self
+  end
+end
