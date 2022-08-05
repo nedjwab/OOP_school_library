f4ebf198ca1487a6c9ae50916fa62ceaf56aa0c2
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-08-05 12:06:01 +0100

    Add create person method

--- app.rb
+++ app.rb
@@ -83,5 +83,22 @@ class App
       key += 1
     end
   end
+
+  def create_person
+    entry = nil
+    print 'Choose (1) to Create a Student or (2) for a Teacher: '
+    until [1, 2].include?(entry)
+      entry = gets.chomp.strip.to_i
+      puts 'Choice is not correct : (1) for Student, (2) for Teacher' unless [1, 2].include?(entry)
+    end
+    case entry
+    when 1
+      create_student
+    when 2
+      create_teacher
+    else
+      p 'Choose between 1 and 2'
+    end
+  end
 end
 # rubocop:enable Metrics/CyclomaticComplexity
